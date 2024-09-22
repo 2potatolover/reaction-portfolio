7e8eb526969514a5ca1ebdb36d70ef8ee9b336a1
Juan Miguel <juan_miguel_francisco@dlsl.edu.ph>
2024-09-22 20:57:51 +0800

    inital

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
         <a1>Juan Miguel G. Francisco</a1>
         <>
         <div class="btn-group" role="group" aria-label="Basic example">
-      <button type="button" class="button">About Me</button>
-      <button type="button" class="button">Projects</button>
-      <button type="button" class="button">Contact Me</button>
+     <a href="p.one"><button type="button" class="button">About Me</button></a>
+     <a href="p.two"><button type="button" class="button">Projects</button> </a>
+     <a href="p.three"><button type="button" class="button">Contact Me</button> </a>
       
      <p class="one"><strong>About Me:</strong></p>
     <p class="two"><strong>Projects:</strong></p>
